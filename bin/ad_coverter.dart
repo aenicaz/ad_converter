59e945febefe73302a93c4a4039d8c9c24f5ca76
Евсеев Артём Владимирович <EvseevAV@sskZvezda.ru>
2023-03-22 16:54:00 +1000

    db implemented, code refactor

--- bin/ad_coverter.dart
+++ bin/ad_coverter.dart
@@ -1,19 +1,69 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:test/test.dart';
+import 'package:ad_coverter/src/user.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main(List<String> arguments) async{
-  var users;
+  File file = File('lib/src/adtest.txt');
+  String futureContent = await file.readAsString(encoding: utf8);
+  var splittedString = futureContent.split(',');
+  String tmp = '';
+  List<String> tmpList = [];
+  List<User> userList = [];
 
-  File file = File('lib/src/active_users.txt');
-  var futureContent = await file.readAsString(encoding: utf8);
-  print(futureContent.toString());
-  // List<String> splittedString;
-  //
-  // print(futureContent);
-  // print('_________________________________________');
-  //
-  // splittedString = futureContent.split(',');
-  // for (var element in splittedString) {print('$element -> ${element.length}');}
+  splittedString.removeAt(splittedString.length - 1);
+  print(splittedString.length);
+
+  for(var i = 0; i < splittedString.length - 2; i++) {
+    if(splittedString[i] == '') splittedString[i] = 'placeholder';
+    for(var j = 0; j < 3; j++) {
+      tmp += splittedString[i + j];
+      tmp += ' ';
+    }
+    User user = User(
+        name: splittedString[i],
+        status: splittedString[i + 1],
+        jobTitle: splittedString[i + 2]);
+    userList.add(user);
+    tmpList.add(tmp);
+    tmp = '';
+    i += 2;
+  }
+  // tmpList.forEach((element) {print('_____________'); print(element);});
+
+  userList.forEach((element) {
+    element.name = element.name?.trimLeft();
+    print(element.name);
+    print(element.status);
+    print(element.jobTitle);
+    print('__________________');
+  });
+
+  print(userList.length.toString());
+  sqfliteFfiInit();
+  var databaseFactory = databaseFactoryFfi;
+  var db = await databaseFactory.openDatabase('C:/distr/proj/SSK_MANAGER/lib/src/asseets/database/sqfile.db');
+  if(db.isOpen) {
+    await db.execute('''
+  CREATE TABLE User (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      status TEXT,
+      jobTitle TEXT
+  )
+  ''');
+
+    for (var i = 0; i < userList.length; i++) {
+      await db.insert(
+          'User',
+          <String, Object?>{'name': userList[i].name, 'status' : userList[i].status, 'jobTitle': userList[i].jobTitle});
+    }
+
+    // print(await db.query('User'));
+
+    db.close();
+
+    print('rdy');
+  }
 }
---
+++ lib/src/user.dart
@@ -0,0 +1,11 @@
+class User{
+  String? name;
+  String? status;
+  String? jobTitle;
+
+  User({
+    this.name,
+    this.status,
+    this.jobTitle
+  });
+}
